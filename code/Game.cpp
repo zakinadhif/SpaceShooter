aa681a4507aabaa16bccb7783e98c40c23915c7d
cowdingus <zaki.nadhif45@gmail.com>
2021-03-29 06:18:39 +0700

    Write game loop and create window

--- code/Game.cpp
+++ code/Game.cpp
@@ -1,11 +1,53 @@
 #include "Game.hpp"
 
+#include "GameStates/MainMenu.hpp"
+
 namespace astro
 {
 
 Game::Game()
+	: m_window(sf::VideoMode(600, 600), "SpaceShooter")
 {
+	m_gameStateManager.push<MainMenu>(m_gameStateManager);
+}
 
+void Game::run()
+{
+	sf::Clock timer;
+	sf::Time elapsed = sf::Time::Zero;
+
+	while (m_window.isOpen() && !m_gameStateManager.isEmpty())
+	{
+		elapsed = timer.restart();
+
+		for (sf::Event event; m_window.pollEvent(event);)
+		{
+			handleEvent(event);
+		}
+
+		update(elapsed.asSeconds());
+
+		m_window.clear();
+		draw();
+		m_window.display();
+
+		m_gameStateManager.tryPop();
+	}
+}
+
+void Game::handleEvent(sf::Event event)
+{
+	m_gameStateManager.handleEvent(event);
+}
+
+void Game::update(float deltaTime)
+{
+	m_gameStateManager.update(deltaTime);
+}
+
+void Game::draw()
+{
+	m_gameStateManager.draw(m_window);
 }
 
 }
--- header/Game.hpp
+++ header/Game.hpp
@@ -12,10 +12,12 @@ public:
 
 	void run();
 private:
+	void handleEvent(sf::Event event);
 	void update(float deltaTime);
 	void draw();
 
-	zfge::GameStateManager gameStateManager;
+	zfge::GameStateManager m_gameStateManager;
+	sf::RenderWindow m_window;
 };
 
 }
