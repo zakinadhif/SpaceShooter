c48a034706d15329e99b9f3e7b2d325056b8f4b5
cowdingus <56494343+cowdingus@users.noreply.github.com>
2021-03-30 20:30:18 +0700

    Write uncompleted ShipInput implementation

--- code/World/Components/ShipInput.cpp
+++ code/World/Components/ShipInput.cpp
@@ -1,10 +1,14 @@
 #include "World/Components/ShipInput.hpp"
 
+#include "Utility/Keyboard.hpp"
+#include "World/Entity.hpp"
+
 namespace astro
 {
 
 void ShipInput::handleEvent(Entity &entity, sf::Event event)
 {
+	using Key = sf::Keyboard::Key;
 }
 
 }
