f6cf131b837ea1813d1ae4c22e25142a0ed204f2
cowdingus <56494343+cowdingus@users.noreply.github.com>
2021-04-01 09:11:48 +0700

    Write ShipPhysics implementation

--- code/World/Components/ShipPhysics.cpp
+++ code/World/Components/ShipPhysics.cpp
@@ -1,5 +1,12 @@
 #include "World/Components/ShipPhysics.hpp"
 
+#include "Utility/Keyboard.hpp"
+#include "Utility/VectorConverter.hpp"
+#include "World/EntityEvent.hpp"
+
+#include <Thor/Vectors.hpp>
+#include <SFML/System/Vector2.hpp>
+
 namespace astro
 {
 
@@ -11,7 +18,47 @@ ShipPhysics::ShipPhysics(b2Body* body)
 
 void ShipPhysics::update(Entity &entity)
 {
+	// Normalize m_direction setted by handleDirectionEvent
+	m_direction = thor::unitVector(m_direction);
 
+	m_body->ApplyForceToCenter(sfVec2ToB2Vec(m_direction *  m_moveForceMultiplier), true);
+
+	// Reset direction vector, because handleDirectionEvent doesn't reset the
+	// direction when key press is released
+	m_direction = {};
+}
+
+void ShipPhysics::recieve(EntityEvent& event)
+{
+	switch (event.type)
+	{
+		case EntityEvent::Type::Direction:
+			handleDirectionEvent(event.direction);
+			break;
+		default:
+			break;
+	}
+}
+
+void ShipPhysics::handleDirectionEvent(EntityEvent::Direction direction)
+{
+	using Direction = EntityEvent::Direction;
+
+	switch (direction)
+	{
+		case Direction::UP:
+			m_direction.y += -1.0f;
+			break;
+		case Direction::DOWN:
+			m_direction.y += 1.0f;
+			break;
+		case Direction::LEFT:
+			m_direction.x += -1.0f;
+			break;
+		case Direction::RIGHT:
+			m_direction.x += 1.0f;
+			break;
+	}
 }
 
 const b2Body* ShipPhysics::getBody() const
--- header/World/Components/ShipPhysics.hpp
+++ header/World/Components/ShipPhysics.hpp
@@ -2,6 +2,10 @@
 
 #include "World/Components/Interfaces/PhysicsComponent.hpp"
 
+#include "World/EntityEvent.hpp"
+
+#include <SFML/System/Vector2.hpp>
+
 namespace astro
 {
 
@@ -11,10 +15,16 @@ public:
 	ShipPhysics(b2Body* body);
 
 	void update(Entity& entity) override;
+	void recieve(EntityEvent& event) override;
 
 	const b2Body* getBody() const override;
 
 private:
+	void handleDirectionEvent(EntityEvent::Direction direction);
+
+	sf::Vector2f m_direction;
+	float m_moveForceMultiplier = 1.0f;
+
 	b2Body* m_body;
 };
 
