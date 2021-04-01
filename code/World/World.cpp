52a7141b78fc2b711d4e9731ec8f4d5b8845ff5c
cowdingus <zaki.nadhif45@gmail.com>
2021-04-01 20:41:33 +0700

    Write World to accomodate game\wsimulation framerate seperation

--- code/World/World.cpp
+++ code/World/World.cpp
@@ -8,22 +8,47 @@ namespace astro
 World::World()
 	: m_physicsWorld({0,0})
 {
-	
+	createPlayerShip({0,0});	
 }
 
 void World::createPlayerShip(const sf::Vector2f &position)
 {
-	::astro::createPlayerShip(*this, position, &m_physicsWorld);
+	Entity entity = ::astro::createPlayerShip(*this, position, &m_physicsWorld);
+
+	m_entities.push_back(std::move(entity));
 }
 
 void World::update(float deltaTime)
 {
+	for (auto& entity : m_entities)
+	{
+		entity.update(deltaTime);
+	}
+}
 
+void World::fixedUpdate(float deltaTime)
+{
+	for (auto& entity : m_entities)
+	{
+		entity.fixedUpdate(deltaTime);
+	}
+
+	m_physicsWorld.Step(deltaTime, m_velocityIterations, m_positionIterations);
 }
 
 void World::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
+	sf::View lastView = target.getView();
+	sf::View worldView({0.f, 0.f}, {16, 9});
 
+	target.setView(worldView);
+
+	for (auto& entity : m_entities)
+	{
+		entity.draw(target, states);
+	}
+
+	target.setView(lastView);
 }
 
 }
--- header/World/World.hpp
+++ header/World/World.hpp
@@ -2,10 +2,17 @@
 
 #include "World/Entity.hpp"
 
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 #include <box2d/b2_world.h>
 
 namespace astro
 {
+
+class Entity;
 
 class World : public sf::Drawable
 {
@@ -16,9 +23,14 @@ public:
 	void createAsteroid(const sf::Vector2f& position);
 
 	void update(float deltaTime);
+	void fixedUpdate(float deltaTime);
+
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 private:
+	const int m_velocityIterations = 6;
+	const int m_positionIterations = 2;
+	
 	b2World m_physicsWorld;
 
 	std::vector<Entity> m_entities;
