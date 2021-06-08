606ab5cba35825a4a34ff1448e998a99cf8cfa2d
cowdingus <56494343+cowdingus@users.noreply.github.com>
2021-06-08 21:07:41 +0700

    Implement createAsteroidBody function and fix position bug

--- code/World/PhysicsBodyFactory.cpp
+++ code/World/PhysicsBodyFactory.cpp
@@ -1,15 +1,19 @@
 #include "World/PhysicsBodyFactory.hpp"
 
+#include "Asteroid/PolygonsGenerator.hpp"
+
 #include <box2d/box2d.h>
+
+#include <vector>
 
 namespace astro
 {
 
-b2Body* createShipBody(b2World* world)
+b2Body* createShipBody(b2World* world, const b2Vec2& position)
 {
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, 0.0f);
+	bodyDef.position.Set(position.x, position.y);
 
 	b2Body* body = world->CreateBody(&bodyDef);
 
@@ -33,9 +37,30 @@ b2Body* createShipBody(b2World* world)
 	return body;
 }
 
-b2Body* createAsteroidBody(b2World* world, thor::ConcaveShape shape)
+b2Body* createAsteroidBody(b2World* world, const b2Vec2& position, const std::vector<Triangleb2d>& triangles)
 {
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(position.x, position.y);
 
+	b2Body* body = world->CreateBody(&bodyDef);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = nullptr;
+	fixtureDef.density = 1.f;
+	fixtureDef.friction = 0.3f;
+
+	for (const auto& triangle : triangles)
+	{
+		b2PolygonShape box2dTriangle;
+		box2dTriangle.Set(triangle.data(), 3);
+
+		fixtureDef.shape = &box2dTriangle;
+
+		body->CreateFixture(&fixtureDef);
+	}
+
+	return body;
 }
 
 }
--- header/World/PhysicsBodyFactory.hpp
+++ header/World/PhysicsBodyFactory.hpp
@@ -3,12 +3,13 @@
 #include <box2d/b2_world.h>
 #include <box2d/b2_body.h>
 
-#include <Thor/Shapes/ConcaveShape.hpp>
+#include <vector>
+#include <array>
 
 namespace astro
 {
 
-b2Body* createShipBody(b2World* world);
-b2Body* createAsteroidBody(b2World* world, thor::ConcaveShape shape);
+b2Body* createShipBody(b2World* world, const b2Vec2& position);
+b2Body* createAsteroidBody(b2World* world, const b2Vec2& position, const std::vector<std::array<b2Vec2, 3>>& triangles);
 
 }
