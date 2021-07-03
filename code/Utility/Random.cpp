dac4e0ad2169e1e65cc2c4e80f507aec4861214c
Ooflamp <zaki.nadhif45@gmail.com>
2021-07-03 08:22:46 +0700

    Write random utility getFloat(min,max) function

--- code/Utility/Random.cpp
+++ code/Utility/Random.cpp
@@ -17,6 +17,11 @@ namespace zfge::Random
 		return uniform_int_dist(getEngine());
 	}
 
+	float getFloat(float min, float max)
+	{
+		return std::uniform_real_distribution{min, max}(getEngine());
+	}
+
 	int getInt(int min, int max)
 	{
 		return std::uniform_int_distribution{min, max}(getEngine());
--- header/Utility/Random.hpp
+++ header/Utility/Random.hpp
@@ -8,6 +8,7 @@ namespace zfge::Random
 float getFloat();
 int   getInt();
 
+float getFloat(float min, float max);
 int   getInt(int min, int max);
 
 std::mt19937& getEngine();
