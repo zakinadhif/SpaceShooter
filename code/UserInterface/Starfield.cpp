7bff268caf6087d6221c65695a43d6ae2767ae4b
Ooflamp <zaki.nadhif45@gmail.com>
2021-07-03 08:21:29 +0700

    Add Starfield class

---
+++ code/UserInterface/Starfield.cpp
@@ -0,0 +1,210 @@
+#include "UserInterface/Starfield.hpp"
+
+#include "Utility/Random.hpp"
+
+#include <SFML/Graphics/RenderTarget.hpp>
+
+#include <cassert>
+#include <cmath>
+
+namespace
+{
+	sf::Color calculateDimmedColor(sf::Color color, float dimmingFactor)
+	{
+		assert(dimmingFactor >= 0.f);
+
+		return
+		{
+			sf::Uint8(std::floor(color.r * (1.f / (dimmingFactor + 1.f)))),
+			sf::Uint8(std::floor(color.g * (1.f / (dimmingFactor + 1.f)))),
+			sf::Uint8(std::floor(color.b * (1.f / (dimmingFactor + 1.f))))
+		};
+	}
+}
+
+namespace astro
+{
+
+Starfield::Starfield()
+	: m_baseColor{255, 255, 255}
+{
+
+}
+
+void Starfield::setSize(sf::Vector2f size)
+{
+	m_size = size;
+	m_needRegeneration = true;
+}
+
+void Starfield::setDirection(Direction direction)
+{
+	m_direction = direction;
+	m_needRegeneration = true;
+}
+
+void Starfield::addLayer(float speed, float dimmingFactor)
+{
+	sf::Vector2f movementVelocity = determineVelocity(speed, m_direction);
+	auto [layer, layerVelocity] = generateLayer(m_baseColor, m_size, movementVelocity, dimmingFactor);
+
+	m_layers.push_back(layer);
+	m_layerVelocities.push_back(layerVelocity);
+}
+
+void Starfield::setLayer(std::size_t index, float speed, float dimmingFactor)
+{
+	sf::Vector2f movementVelocity = determineVelocity(speed, m_direction);
+	auto [layer, layerVelocity] = generateLayer(m_baseColor, m_size, movementVelocity, dimmingFactor);
+
+	m_layers.at(index) = layer;
+	m_layerVelocities.at(index) = layerVelocity;
+}
+
+void Starfield::removeLayer(std::size_t index)
+{
+	m_layers.erase(m_layers.begin() + index);
+	m_layerVelocities.erase(m_layerVelocities.begin() + index);
+}
+
+sf::Vector2f Starfield::determineVelocity(float speed, Direction direction)
+{
+	// Calculate view movement velocity
+	sf::Vector2f velocity;
+
+	switch (direction)
+	{
+		case Direction::Up:
+			velocity = {0.f, speed};
+			break;
+		case Direction::Down:
+			velocity = {0.f, -speed};
+			break;
+		case Direction::Left:
+			velocity = {speed, 0.f};
+			break;
+		case Direction::Right:
+			velocity = {-speed, 0.f};
+			break;
+	}
+
+	return velocity;
+}
+
+float Starfield::determineSpeed(sf::Vector2f velocity)
+{
+	if (velocity.x == 0)
+	{
+		return std::abs(velocity.y);
+	}
+	
+	return std::abs(velocity.x);
+}
+
+std::tuple<Starfield::Layer, Starfield::LayerVelocity>
+Starfield::generateLayer(sf::Color color, sf::Vector2f size, sf::Vector2f viewVelocity, float dimmingFactor)
+{
+	// Generate starfield -- two times as big as the screen size so it can be scrolled infinitely
+	const sf::Vector2f starfieldSize     {size.x * 2, size.y * 2};
+	const sf::Vector2f starfieldViewSize {size.x, size.y};
+
+	const sf::Color starfieldColor = calculateDimmedColor(color, dimmingFactor);
+
+	sf::VertexArray starfield = generateStars(starfieldColor, starfieldSize);
+	sf::View starfieldView({0, 0, starfieldViewSize.x, starfieldViewSize.y});
+
+	return {{starfield, starfieldView}, {viewVelocity}};
+}
+
+sf::VertexArray Starfield::generateStars(sf::Color color, sf::Vector2f size)
+{
+	const std::size_t starCount = 500u;
+
+	sf::VertexArray stars;
+	stars.setPrimitiveType(sf::Points);
+	stars.resize(starCount);
+
+	for (std::size_t x = 0; x < starCount; ++x)
+	{
+		sf::Vector2f starPosition {
+			zfge::Random::getFloat(0, size.x),
+			zfge::Random::getFloat(0, size.y)
+		};
+
+		stars[x].color = color;
+		stars[x].position = starPosition;
+	}
+
+	return stars;
+}
+
+void Starfield::regenerateAllLayers()
+{
+	// Preserve star colors
+	std::vector<sf::Color> colors;
+
+	for (const auto& [starfield, _] : m_layers)
+	{
+		if (starfield.getVertexCount() == 0)
+		{
+			continue;
+		}
+
+		colors.push_back(starfield[0].color);
+	}
+
+	// Preserve star velocities
+	std::vector<sf::Vector2f> velocities = m_layerVelocities;
+
+	// Rotate all star velocity vectors to specified direction
+	for (auto& velocity : velocities)
+	{
+		float speed = determineSpeed(velocity);
+		velocity = determineVelocity(speed, m_direction);
+	}
+
+	// Regenerate all layers
+	std::size_t layerCount = m_layers.size();
+
+	m_layers.clear();
+	m_layerVelocities.clear();
+
+	for (std::size_t x = 0; x < layerCount; ++x)
+	{
+		auto [layer, layerVelocity] = generateLayer(colors[x], m_size, velocities[x]);
+		m_layers.push_back(layer);
+		m_layerVelocities.push_back(layerVelocity);
+	}
+}
+
+void Starfield::update(float deltaTime)
+{
+	if (m_needRegeneration)
+	{
+		regenerateAllLayers();
+		m_needRegeneration = false;
+	}
+
+	for (std::size_t x = 0; x < m_layers.size(); ++x)
+	{
+		const auto velocity = m_layerVelocities.at(x);
+		auto& [_, view] = m_layers.at(x);
+
+		view.move(velocity.x * deltaTime, velocity.y * deltaTime);
+	}
+}
+
+void Starfield::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+	sf::View oldView = target.getView();
+
+	for (const auto& [starfield, view] : m_layers)
+	{
+		target.setView(view);
+		target.draw(starfield);
+	}
+
+	target.setView(oldView);
+}
+
+}
---
+++ header/UserInterface/Starfield.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <SFML/Graphics/View.hpp>
+
+namespace astro
+{
+
+class Starfield : public sf::Drawable
+{
+public:
+	enum class Direction
+	{
+		Up,
+		Down,
+		Left,
+		Right
+	};
+
+public:
+	Starfield();
+
+	void setSize(sf::Vector2f size);
+	void setDirection(Direction direction);
+
+	void addLayer(float speed, float dimmingFactor);
+	void setLayer(std::size_t index, float speed, float dimmingFactor);
+	void removeLayer(std::size_t index);
+
+	void update(float deltaTime);
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+private:
+	typedef std::tuple<sf::VertexArray, sf::View> Layer;
+	typedef sf::Vector2f LayerVelocity;
+
+	std::vector<Layer> m_layers;
+	std::vector<LayerVelocity> m_layerVelocities;
+
+	sf::Color m_baseColor {sf::Color::White};
+	sf::Vector2f m_size   {800, 800};
+	Direction m_direction {Direction::Down};
+
+	bool m_needRegeneration = true;
+
+	std::tuple<Layer, LayerVelocity> generateLayer(
+		sf::Color color,
+		sf::Vector2f size,
+		sf::Vector2f viewVelocity,
+		float dimmingFactor = 0.f
+	);
+
+	sf::VertexArray generateStars(sf::Color color, sf::Vector2f size);
+
+	sf::Vector2f determineVelocity(float speed, Direction direction);
+	float determineSpeed(sf::Vector2f velocity);
+	void regenerateAllLayers();
+};
+
+}
