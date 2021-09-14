bdb6f0106531885ae102f93b999cfa1a23ee17e9
ZakiNadhif <zaki.nadhif45@gmail.com>
2021-09-14 16:11:43 +0700

    Update Components.hpp includes

--- header/World/Components/Components.hpp
+++ header/World/Components/Components.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "TransformComponent.hpp"
 #include "IdentifierComponent.hpp"
 #include "MeshComponent.hpp"
 #include "RigidBodyComponent.hpp"
 #include "OwningMeshComponent.hpp"
 #include "NativeScriptComponent.hpp"
+#include "BulletComponent.hpp"
+#include "NativeScriptComponent.hpp"
+#include "AsteroidComponent.hpp"
