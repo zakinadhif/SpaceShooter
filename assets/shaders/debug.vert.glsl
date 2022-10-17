403bf3079aeaf8f6f60e65b3aa082ed8012f6d38
zakinadhif <zaki.nadhif45@gmail.com>
2022-10-17 09:05:35 +0700

    Get rid of texture related LoC in debug shader

--- assets/shaders/debug.vert.glsl
+++ assets/shaders/debug.vert.glsl
@@ -2,9 +2,7 @@ void main()
 {
     // transform the vertex position
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-
-    // transform the texture coordinates
-    gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;
+    gl_Position.y *= -1.0;
 
     // forward the vertex color
     gl_FrontColor = gl_Color;
